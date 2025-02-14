360b96fa01ec0db84394e03fab55e792799a9912
manuel <moncadajuanmanuel25@gmail.com>
2025-02-14 14:05:44 -0500

    operaciones basicas

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,10 @@
 fun main() {
-println("hola mundo")
-println("juan manuel moncada felix")
+    println("hola mundo")
+    println("juan manuel moncada felix")
     println("caminar")
+
+    println("esta es mi rama de desarrollo")
+    println("suma")
+    println("resta")
+
 }
